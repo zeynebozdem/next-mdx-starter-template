795ec280986ba5053dfa6c965871df3f26bec2c0
Zeynep Özdem Özdem <zeynebozdem@gmail.com>
2021-03-18 00:40:58 +0300

    .

---
+++ components/Post.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { HeadPost } from "./HeadPost";
+
+export const Post = ({ post }) => {
+  const {
+    link,
+    module: { meta }
+  } = post;
+
+  return (
+    <article>
+      <HeadPost meta={meta} />
+      <Link href={"/blog" + link}>
+        <a>Read more &rarr;</a>
+      </Link>
+      <style jsx>
+        {`
+          article {
+            margin-bottom: 3rem;
+          }
+        `}
+      </style>
+    </article>
+  );
+};
